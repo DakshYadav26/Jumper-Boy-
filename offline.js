8b10a68ba3b91ffc89ad4bf4265f04f2c059f860
DakshYadav26 <daksh.yadav1@ais.amity.edu>
2021-06-30 19:31:59 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1625061575,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/platform2-sheet0.png",
+		"images/gold-sheet0.png",
+		"images/turret-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/mainbackground.png",
+		"images/game.png",
+		"media/5802890126426112.m4a",
+		"media/5802890126426112.ogg",
+		"media/4664333924040704.m4a",
+		"media/4664333924040704.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
